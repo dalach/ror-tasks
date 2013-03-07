1b77cfb433a459807155aceb019ebcc70c3e45ff
Wiktoria Dalach <09_dalach@wierzba.wzks.uj.edu.pl>
2013-03-07 15:16:32 +0100

    two test added

--- task-1/lib/todo_list.rb
+++ task-1/lib/todo_list.rb
@@ -2,5 +2,23 @@ class TodoList
 
   # Initialize the TodoList with +items+ (empty by default).
   def initialize(items=[])
+  	@items = items
   end
+
+  def empty?
+    @items.empty?
+  end
+
+  def size
+    if @items.empty?
+       0 
+    else
+       @items.length
+    end
+  end
+  def <<(item_description) 
+    not @items.empty?
+ 
+  end
+
 end
